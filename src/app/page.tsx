23c7cc80ee791bdd62d27181c852085f86d56e29
Nitin Sahu <sahunitin970@gmail.com>
2025-08-05 11:36:34 +0530

    Implement code changes to enhance functionality and improve performance

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,8 +12,8 @@ export default function Home() {
       <CoursesSection />
       <WhatYouGetSection />
       <TestimonialsSection />
-      <LearningStatsSection />
       <JoinLearnersSection />
+      <LearningStatsSection />
       <Footer />
       <CopyrightSection />
     </div>
--- src/components/HomePageSections/JoinLearnersSection.tsx
+++ src/components/HomePageSections/JoinLearnersSection.tsx
@@ -2,23 +2,23 @@ import Image from "next/image";
 
 const JoinLearnersSection = () => {
   return (
-    <section className="w-full py-16 px-5 xl:px-0 bg-white">
-      <div className="max-w-7xl mx-auto flex flex-col md:flex-row items-center justify-between gap-8">
+    <section className="w-full py-10 pl-5">
+      <div className="max-w-7xl mx-auto flex flex-row items-center justify-between gap-8 xl:px-10">
         {/* Left: Text */}
-        <div className="w-full md:w-1/2 text-center md:text-left">
-          <h2 className="text-4xl md:text-5xl font-semibold text-gray-900 leading-tight mb-4">
-            Join over <span className="text-blue-600 font-bold">10,000</span><br /> learners now
+        <div className="w-[50vw] text-left">
+          <h2  className="text-gray-900 leading-tight font-light mb-4 text-lg text-nowrap sm:text-3xl md:text-4xl lg:text-5xl">
+            Join over <span className="text-blue-600 font-semibold">10,000</span><br /> learners now
           </h2>
         </div>
         {/* Right: Illustration */}
-        <div className="w-full md:w-1/2">
-          <div className="relative w-full max-w-full h-[260px]">
+        <div className="w-[50vw]  flex items-center justify-center">
+          <div className="relative w-[44vw] h-[20vw] max-w-[600px] max-h-[250px]">
             <Image
-              src="/banner.png"
+              src="/banner2.png"
               alt="Join over 10,000 learners banner"
               fill
               style={{ objectFit: "cover" }}
-              className="rounded-xl w-full h-full"
+              className="rounded-xl"
               priority
             />
           </div>
