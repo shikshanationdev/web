c8e21420cc3f2bbdd9c9cedab71a61e58c05f255
Nitin Sahu <sahunitin970@gmail.com>
2025-08-12 12:57:35 +0530

    feat(Media): restructure media components and move to new directory for better organization

--- src/app/media/page.tsx
+++ src/app/media/page.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
-import MediaHero from '../../components/MediaHero'
+import MediaHero from '../../components/media/MediaHero'
 
-import MediaContact from '@/components/MediaContact'
-import MediaGrid from '@/components/MediaGrid'
+import MediaContact from '@/components/media/MediaContact'
+import MediaGrid from '@/components/media/MediaGrid'
 
 const Media = () => {
   return <div className='max-w-[1536px] mx-auto px-4'>
