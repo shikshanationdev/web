f162e28f4356353752785b0ee3add58da0dbde51
Nitin Sahu <sahunitin970@gmail.com>
2025-09-10 12:56:25 +0530

    Privacy Policy page complete

--- src/app/privacy-policy/page.tsx
+++ src/app/privacy-policy/page.tsx
@@ -1,9 +1,17 @@
-import React from 'react'
+import React from 'react';
+import Banner from '@/components/Banner';
+import PrivacyPolicyContent from '@/components/privacy/PrivacyPolicyContent';
 
-const page = () => {
+const PrivacyPolicyPage = () => {
   return (
-    <div className='max-w-[1536px] mx-auto'>page</div>
-  )
-}
+    <div className='max-w-[1536px] mx-auto'>
+      <Banner
+        heading="Privacy Policy"
+        subtitle="Your privacy is important to us"
+      />
+      <PrivacyPolicyContent />
+    </div>
+  );
+};
 
-export default page
+export default PrivacyPolicyPage;
